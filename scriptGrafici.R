a8b6a049a0b133ea070b111bf9b38cfbb21f9f56
andreagurioli1995 <andrea.gurioli2@studio.unibo.it>
2020-02-22 18:03:12 +0100

    update

--- scriptGrafici.R
+++ scriptGrafici.R
@@ -15,3 +15,19 @@ DeviazioneStandardGP<-aggregate(pingtime_SEC ~ gasprice_GWEI, data=Datas, FUN=sd
 DeviazioneStandardGP
 DeviazioneStandardGP<-rename(DeviazioneStandardGP, deviazione_standard='pingtime_SEC')
 plot(DeviazioneStandardGP$gasprice_GWEI,DeviazioneStandardGP$deviazione_standard,type='h',col='blue',lwd=4)
+
+
+ModelloDecentralizzato<- seq(0,13,1.34)
+ModelloCentralizzato<- c(6.95,6.95,6.95,6.95,6.95,6.95,6.95,6.95,6.95,6.95)
+giorni<-c(1,2,3,4,5,6,7,8,9,10)
+dataframe1<- tbl_df(data.frame(giorni,ModelloDecentralizzato,ModelloCentralizzato))
+
+
+ggplot()+
+  geom_line(data=dataframe1,aes(y=ModelloDecentralizzato,x= giorni,colour="darkblue"),size=1 )+
+  geom_line(data=dataframe1,aes(y=ModelloCentralizzato,x= giorni,colour="green"),size=1) +  scale_x_continuous(breaks = seq(0,10,1)) +
+  scale_color_discrete(name = "Tipologia modello", labels = c("Modello decentralizzato", "Modello centralizzato")) + labs(
+    x = "Giornate trascorse",
+    y = "Costi complessivi (€)"
+  )   
+
